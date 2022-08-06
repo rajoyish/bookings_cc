fc0773543788617eed55308950709d04893c517c
Rajesh Budhathoki <rajoyish@gmail.com>
2022-08-06 19:24:26 +0545

    Adding an appointment filter

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Bookings\Filters\AppointmentFilter;
 use App\Models\Service;
 use App\Models\Schedule;
 use Carbon\CarbonInterval;
@@ -10,6 +11,7 @@ use App\Bookings\TimeSlotGenerator;
 use App\Models\ScheduleUnavailability;
 use App\Bookings\Filters\UnavailabilityFilter;
 use App\Bookings\Filters\SlotsPassedTodayFilter;
+use App\Models\Appointment;
 
 class BookingController extends Controller
 {
@@ -19,15 +21,17 @@ class BookingController extends Controller
         // $slots = CarbonInterval::days(1)
         //     ->toPeriod(now(), now()->addYear());
 
-        $schedule = Schedule::find(2);
-        $service = Service::find(2);
+        $schedule = Schedule::find(1);
+        $service = Service::find(1);
 
+        $appointments = Appointment::whereDate('date', '2022-07-29')->get();
 
         $slots = (new TimeSlotGenerator($schedule, $service))
             ->applyFilters(
                 [
                     new SlotsPassedTodayFilter(),
-                    new UnavailabilityFilter($schedule->unavailabilities)
+                    new UnavailabilityFilter($schedule->unavailabilities),
+                    new AppointmentFilter($appointments)
 
                 ]
             )
