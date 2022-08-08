75bc4af17cc8245a5d0083db854757e49b4e365c
Rajesh Budhathoki <rajoyish@gmail.com>
2022-08-08 08:30:31 +0545

    Scaffolding the booking form UI

---
+++ resources/views/livewire/create-booking.blade.php
@@ -0,0 +1,8 @@
+<div class="bg-gray-200 max-w-sm mx-auto m-6 p-5 rounded-lg">
+    <form>
+        <label for="service" class="inline-block text-gray-700 font-bold mb-2">Select Service</label>
+        <select name="service" id="service" class="bg-white h-10 w-full border-none rounded-lg">
+            <option value="">Codding Session</option>
+        </select>
+    </form>
+</div>
